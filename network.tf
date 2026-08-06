8a94f7df840f135f633099802c323a37238f776a
Phumthawan Nokun <knzphumthawan@gmail.com>
2026-08-06 15:55:50 +0700

    add vpc

---
+++ network.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.6.1"
+
+  name = "${var.project_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = local.availability_zones
+  public_subnets  = local.public_subnet_cidrs
+  private_subnets = local.private_subnet_cidrs
+
+  enable_dns_hostnames = true
+
+  // only 1 nat gateway
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  private_subnet_tags = {
+    "karpenter.sh/discovery" = local.cluster_name
+  }
+}
