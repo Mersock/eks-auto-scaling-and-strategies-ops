9b853aa75d67b0431845da9eddb5cccbaf427df7
Phumthawan Nokun <knzphumthawan@gmail.com>
2026-08-06 16:54:22 +0700

    bind load balancer role and policy

--- controller-iam.tf
+++ controller-iam.tf
@@ -13,3 +13,25 @@ data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role" {
     }
   }
 }
+
+resource "aws_iam_role" "aws_load_balancer_controller" {
+  name               = "${var.project_name}-aws-load-balancer-controller"
+  assume_role_policy = data.aws_iam_policy_document.aws_load_balancer_controller_assume_role.json
+}
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name   = "${var.project_name}-aws-load-balancer-controller"
+  policy = file("${path.module}/policies/aws-load-balancer-controller-v3.4.0.json")
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller" {
+  role       = aws_iam_role.aws_load_balancer_controller.name
+  policy_arn = aws_iam_policy.aws_load_balancer_controller.arn
+}
+
+resource "aws_eks_pod_identity_association" "aws_load_balancer_controller" {
+  cluster_name    = module.eks.cluster_name
+  namespace       = local.aws_load_balancer_controller_namespace
+  service_account = local.aws_load_balancer_controller_service_account
+  role_arn        = aws_iam_role.aws_load_balancer_controller.arn
+}
