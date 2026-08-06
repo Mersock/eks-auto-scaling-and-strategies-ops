edb03bf1bf4426a247011d1bffed5d23a0f2271b
Phumthawan Nokun <knzphumthawan@gmail.com>
2026-08-06 16:42:49 +0700

    module karpenter

---
+++ karpenter.tf
@@ -0,0 +1,9 @@
+module "karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "21.24.1"
+
+  cluster_name = module.eks.cluster_name
+  namespace    = local.karpenter_namespace
+
+  tags = local.common_tags
+}
--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,22 @@ output "configure_kubectl_command" {
 }
 
 output "node_security_group_id" {
-  description = "security group EKS and Karpenter nodes."
+  description = "security group EKS and Karpenter nodes"
   value       = module.eks.node_security_group_id
 }
+
+output "karpenter_controller_role_arn" {
+  description = "IAM role for Karpenter controller EKS Pod Identity"
+  value       = module.karpenter.iam_role_arn
+}
+
+output "karpenter_node_role_name" {
+  description = "IAM role for Karpenter EC2NodeClass use for provisioned nodes"
+  value       = module.karpenter.node_iam_role_name
+}
+
+output "karpenter_interruption_queue_name" {
+  description = "SQS queue name for Karpenter events"
+  value       = module.karpenter.queue_name
+}
+
