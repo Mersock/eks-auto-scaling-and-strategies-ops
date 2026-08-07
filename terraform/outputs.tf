adedad2841d1c9a25260e9414318c7904ce37aa0
Phumthawan Nokun <knzphumthawan@gmail.com>
2026-08-07 10:48:26 +0700

    output for bootstarp argocd

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -33,3 +33,32 @@ output "karpenter_interruption_queue_name" {
   value       = module.karpenter.queue_name
 }
 
+output "aws_load_balancer_controller_role_arn" {
+  description = "IAM role AWS Load Balancer Controller in EKS Pod Identity"
+  value       = aws_iam_role.aws_load_balancer_controller.arn
+}
+
+output "nlb_dns_name" {
+  description = "DNS public Network Load Balancer"
+  value       = aws_lb.aws_lb_eks.dns_name
+}
+
+output "application_url" {
+  description = "HTTP URL application"
+  value       = "http://${aws_lb.aws_lb_eks.dns_name}"
+}
+
+output "target_group_arn" {
+  description = "Target group arn Argo CD"
+  value       = aws_lb_target_group.eks_pod_apps.arn
+}
+
+output "argocd_public_url_command" {
+  description = "Command public Argo CD URL from NLB"
+  value       = "echo http://$(kubectl --namespace ${local.argocd_namespace} get service argocd-server-public --output jsonpath='{.status.loadBalancer.ingress[0].hostname}')"
+}
+
+output "argocd_admin_password_command" {
+  description = "Command Argo CD administrator password"
+  value       = "kubectl --namespace ${local.argocd_namespace} get secret argocd-initial-admin-secret --output jsonpath='{.data.password}' | base64 --decode; echo"
+}
