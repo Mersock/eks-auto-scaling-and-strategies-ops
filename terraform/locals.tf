685cbaffbb0944803fffcfe7d97e6b2a8ef3f990
Phumthawan Nokun <knzphumthawan@gmail.com>
2026-08-08 09:51:09 +0700

    adjust eks cluster

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,31 +1,36 @@
 locals {
-  cluster_name = "${var.project_name}-cluster"
+  cluster_name = var.cluster_name
 
-  // 2 AZ
-  availability_zones = slice(data.aws_availability_zones.available.names, 0, 2)
+  // 3 AZ
+  availability_zones = slice(data.aws_availability_zones.available.names, 0, 3)
 
   public_subnet_cidrs = [
     cidrsubnet(var.vpc_cidr, 8, 0),
     cidrsubnet(var.vpc_cidr, 8, 1),
+    cidrsubnet(var.vpc_cidr, 8, 2),
   ]
 
   private_subnet_cidrs = [
     cidrsubnet(var.vpc_cidr, 8, 10),
     cidrsubnet(var.vpc_cidr, 8, 11),
+    cidrsubnet(var.vpc_cidr, 8, 12),
   ]
 
   // low size for demo only
   system_node_group = {
+    // instance_types = ["t3.large"]
     instance_types = ["t3.medium"]
-    min_size       = 1
-    desired_size   = 2
-    max_size       = 2
+    min_size       = 3
+    desired_size   = 3
+    max_size       = 3
   }
 
   argocd_namespace = "argocd"
 
   karpenter_namespace       = "karpenter"
   karpenter_service_account = "karpenter-sa"
+  karpenter_queue_name      = "${local.cluster_name}-karpenter"
+  karpenter_node_role_name  = "KarpenterNodeRole-${local.cluster_name}"
 
   aws_load_balancer_controller_namespace       = "kube-system"
   aws_load_balancer_controller_service_account = "aws-load-balancer-controller-sa"
